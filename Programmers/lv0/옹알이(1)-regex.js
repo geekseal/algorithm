542bd74dd0f1605810504a734d49ea9923f1b8fa
Kihyun Ryu <khryu0610@gmail.com>
2023-05-05 19:41:10 +0900

    옹알이 using regex

---
+++ Programmers/lv0/옹알이(1)-regex.js
@@ -0,0 +1,10 @@
+function solution(babbling) {
+    let answer = 0;
+    const regex = /^(aya|ye|woo|ma)+$/;
+
+    babbling.forEach((word) => {
+        if (regex.test(word)) answer++;
+    });
+
+    return answer;
+}
