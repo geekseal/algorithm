ed8796660d04cc2b4ed1f69e9e406f33532d666e
Kihyun Ryu <khryu0610@gmail.com>
2023-05-26 11:22:39 +0900

    피보나치 수

---
+++ Programmers/lv2/피보나치 수.js
@@ -0,0 +1,22 @@
+function solution(n) {
+  let fiboSequence = [1, 0];
+  while (n >= 2) {
+    [a, b] = fiboSequence;
+    fiboSequence = [(a + b) % 1234567, a % 1234567];
+    n--;
+  }
+  return fiboSequence[0];
+}
+
+/*
+// RangeError: Maximum call stack size exceeded
+const F = n => {
+  if (n <= 1) return [n, 0];
+
+  [a, b] = F(n - 1);
+  return [(a + b) % 1234567, a % 1234567];
+};
+*/
+
+console.log(solution(3));
+console.log(solution(5));
