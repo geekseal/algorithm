b341668fd7c86a0fb51bda715492b766b65204de
Kihyun Ryu <khryu0610@gmail.com>
2023-05-29 21:27:18 +0900

    k번째 수

---
+++ Programmers/lv1/k번째수.js
@@ -0,0 +1,18 @@
+function solution(array, commands) {
+  const answer = commands.map(
+    ([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
+  );
+
+  return answer;
+}
+
+console.log(
+  solution(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3],
+    ]
+  )
+);
