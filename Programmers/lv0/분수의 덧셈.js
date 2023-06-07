b932137a49849998ad634aadf1d867c6fd2edcf2
Kihyun Ryu <khryu0610@gmail.com>
2023-06-07 09:01:19 +0900

    분수의 덧셈 주석 추가

--- Programmers/lv0/분수의 덧셈.js
+++ Programmers/lv0/분수의 덧셈.js
@@ -1,16 +1,18 @@
 function solution(numer1, denom1, numer2, denom2) {
-    const denom = denom1 * denom2;
-    const numer = numer1 * denom2 + numer2 * denom1;
-    const gcd = GCD(denom, numer);
-    return [numer / gcd, denom / gcd];
+  const denom = denom1 * denom2;
+  const numer = numer1 * denom2 + numer2 * denom1;
+  const gcd = GCD(denom, numer);
+  return [numer / gcd, denom / gcd];
 }
 
+// 최대공약수
 const GCD = (a, b) => {
-    if (a < b) [a, b] = [b, a];
+  if (a < b) [a, b] = [b, a];
 
-    return b ? GCD(b, a % b) : a;
+  return b ? GCD(b, a % b) : a;
 };
 
 console.log(solution(9, 2, 1, 3));
 
+// 최소공배수
 const LCM = (a, b) => (a * b) / GCD(a, b);
