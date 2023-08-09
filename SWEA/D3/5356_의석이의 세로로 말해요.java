a9fd0768f5df3ceccb08ca325ca9482f571857d0
Kihyun Ryu <khryu0610@gmail.com>
2023-08-09 10:39:53 +0900

    Create 5356_의석이의 세로로 말해요.java

---
+++ SWEA/D3/5356_의석이의 세로로 말해요.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+	
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T = Integer.parseInt(sc.nextLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			ArrayList<String> list = new ArrayList<String>();
+			
+			for (int i=0; i<5; i++) {
+				String[] chars = sc.nextLine().split("");
+				for (int j=0; j<chars.length; j++) {
+					try {
+						list.set(j, list.get(j) + chars[j]);
+					} catch (IndexOutOfBoundsException e) {
+						list.add(chars[j]);
+					}
+				}
+			}
+			
+			String answer = "";
+			
+			for (String str: list) {
+				answer += str;
+			}
+			System.out.printf("#%s %s\n", test_case, answer);
+			
+		}
+		sc.close();
+	}
+}
