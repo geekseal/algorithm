554d3f1f9897f33841dd0e6de261033615b432b9
Kihyun Ryu <khryu0610@gmail.com>
2023-09-25 00:07:48 +0900

    Create combination

---
+++ Impl/src/combination/Combination.java
@@ -0,0 +1,31 @@
+package combination;
+
+import java.util.Arrays;
+
+public class Combination {
+	static int[] arr;
+	static int N;
+	static int K;
+	static int[] sel;
+	
+	private static void combination(int idx, int sidx) {
+		if (sidx==K) {
+			System.out.println(Arrays.toString(sel));
+			return;
+		}
+		
+		for (int i=idx; i<=N-K+sidx; i++) {
+			sel[sidx] = arr[i];
+			combination(i+1, sidx+1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		N = 7;
+		K = 6;
+		arr = new int[] {1,2,3,4,5,6,7};
+		sel = new int[K];
+		
+		combination(0, 0);
+	}
+}
